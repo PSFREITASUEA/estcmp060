157b34e99fab1f4091f7b7a1ac0fd7f63de8d73e
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-05-07 19:23:37 -0400

    #Update: draw squares

--- FibonacciFractal.py
+++ FibonacciFractal.py
@@ -15,17 +15,51 @@ def draw_first_square(fibonacci_second_element, render_zoom_factor):
 def fibonacci_rendering(fibonacci_iteration_quantity, render_zoom_factor):
     fibonacci_first_element = 0
     fibonacci_second_element = 1
+    square_a_size_side = fibonacci_first_element
+    square_b_size_side = fibonacci_second_element
 
     turtle_pen.pencolor("blue")
 
     draw_first_square(fibonacci_second_element, render_zoom_factor)
+
+    square_a_size_side, square_b_size_side = calculate_fibo(square_a_size_side, square_b_size_side)
+
+    for i in range(1, fibonacci_iteration_quantity):
+        draw_square(render_zoom_factor, square_a_size_side, square_b_size_side)
+        square_a_size_side, square_b_size_side = calculate_fibo(square_a_size_side, square_b_size_side)
+
+
+def setup_pen_to_initial_position_and_set_color_to_red(render_zoom_factor):
+    turtle_pen.penup()
+    turtle_pen.setposition(render_zoom_factor, 0)
+    turtle_pen.seth(0)
+    turtle_pen.speed(10)
+    turtle_pen.pendown()
+    turtle_pen.pencolor("red")
+
+
+def calculate_fibo(square_a_size_side, square_b_size_side):
+    temp = square_b_size_side
+    square_b_size_side = square_b_size_side + square_a_size_side
+    square_a_size_side = temp
+    return square_a_size_side, square_b_size_side
+
+
+def draw_square(render_zoom_factor, square_a_size_side, square_b_size_side):
+    turtle_pen.backward(square_a_size_side * render_zoom_factor)
+    turtle_pen.right(90)
+    turtle_pen.forward(square_b_size_side * render_zoom_factor)
+    turtle_pen.left(90)
+    turtle_pen.forward(square_b_size_side * render_zoom_factor)
+    turtle_pen.left(90)
+    turtle_pen.forward(square_b_size_side * render_zoom_factor)
 
 
 if __name__ == '__main__':
 
     zoom_factor: float = 25
     iteration_quantity: int = 1
-    turtle_pen_speed: int = 10
+    turtle_pen_speed: int = 1
 
     iteration_quantity = int(input('Insira o número de interações (deve ser > 1): '))
 
