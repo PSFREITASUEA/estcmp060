2dea6b39874c6e38031dbbd45cba36cadd8c4f70
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-05-07 15:00:18 -0400

    #Feat: add user input to define iteration quantity

--- FibonacciFractal.py
+++ FibonacciFractal.py
@@ -1,6 +1,13 @@
-import turtle
 import math
+import turtle
 
 if __name__ == '__main__':
-    print('Hello World!!')
+
+    zoom_factor: float = 8.6
+    iteration_quantity: int = 1
+
+    iteration_quantity = int(input('Insira o número de interações (deve ser > 1): '))
+
+    while iteration_quantity < 1:
+        iteration_quantity = int(input('Insira o número de interações (deve ser > 1): '))
 
