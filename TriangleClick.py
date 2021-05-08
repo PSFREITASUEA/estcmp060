d25077ede3167fec5fe01b397ad74893c4d061d1
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-05-08 17:39:29 -0400

    #Feature: draw triangle

--- TriangleClick.py
+++ TriangleClick.py
@@ -6,6 +6,11 @@ def draw_triangle(x_axis_coordinate, y_axis_coordinate):
     turtle_pen.goto(x_axis_coordinate, y_axis_coordinate)
     turtle_pen.pendown()
 
+    for i in range(0, 3):
+        turtle_pen.forward(100)
+        turtle_pen.left(120)
+        turtle_pen.forward(100)
+
 
 if __name__ == '__main__':
     turtle_screen = turtle.Screen()
