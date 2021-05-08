470ad6bbca76cb4d30f5016b4dff7344fcb4a416
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-05-08 17:36:56 -0400

    #Fix: pewdown the correct pen

--- TriangleClick.py
+++ TriangleClick.py
@@ -4,12 +4,13 @@ import turtle
 def draw_triangle(x_axis_coordinate, y_axis_coordinate):
     turtle_pen.penup()
     turtle_pen.goto(x_axis_coordinate, y_axis_coordinate)
-    turtle.pendown()
+    turtle_pen.pendown()
 
 
 if __name__ == '__main__':
     turtle_screen = turtle.Screen()
     turtle_pen = turtle.Turtle()
+    turtle_pen.pencolor("black")
     turtle.onscreenclick(draw_triangle, 1)
     turtle.listen()
     turtle.done()
