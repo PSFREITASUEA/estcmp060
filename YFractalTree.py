e6db23b934e168f755b8c856cb4269a158340d41
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-05-08 16:49:11 -0400

    #Feature: instantiate turtle pen

---
+++ YFractalTree.py
@@ -0,0 +1,9 @@
+import turtle
+from turtle import Turtle
+
+
+if __name__ == '__main__':
+    turtle_pen: Turtle = turtle.Turtle()
+    turtle_pen.speed(2)
+    turtle_pen.right(-90)
+    angle = 30
