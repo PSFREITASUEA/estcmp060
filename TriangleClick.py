5b8b23db5547b2539e706f4e71b083161cd9f8ca
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-05-08 17:27:44 -0400

    #Feature: move turtle pen to click position

--- TriangleClick.py
+++ TriangleClick.py
@@ -1,8 +1,10 @@
 import turtle
 
 
-def draw_triangle(x, y):
-    print(x, y)
+def draw_triangle(x_axis_coordinate, y_axis_coordinate):
+    turtle_pen.penup()
+    turtle_pen.goto(x_axis_coordinate, y_axis_coordinate)
+    turtle.pendown()
 
 
 if __name__ == '__main__':
